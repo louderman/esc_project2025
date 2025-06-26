3ead41e753ea1fa47e4c2751d029722a6ab6a663
itsmeox <ongxuan1028@gmail.com>
2025-06-26 17:05:46 +0800

    Add searchbar to listing page

--- client/src/pages/ListingPage.tsx
+++ client/src/pages/ListingPage.tsx
@@ -1,4 +1,12 @@
+import SearchBar from '../components/listing/SearchBar';
 import styles from './listingpage.module.css';
 export default function ListingPage() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.searchbarSection}>
+        <SearchBar />
+      </div>
+      <div className={styles.contentSection}></div>
+    </div>
+  );
 }
