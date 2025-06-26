e515756892cdd9062f40deeeac8e151839e1cb80
itsmeox <ongxuan1028@gmail.com>
2025-06-26 21:38:01 +0800

    Complete listing searchbar frontend

--- client/src/components/listing/SearchBar.tsx
+++ client/src/components/listing/SearchBar.tsx
@@ -27,6 +27,7 @@ export default function SearchBar() {
           placeholder='Guest count'
         />
       </div>
+      <button className={styles.searchButton}>Find Hotels</button>
     </div>
   );
 }
