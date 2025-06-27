57b7a86b0f959649a3a9504f39f3caf94b8c1c0e
ItsMeOX <ongxuan1028@gmail.com>
2025-06-27 20:55:57 +0800

    Setup API route for like fuzzy matching

--- server/routes/destination.ts
+++ server/routes/destination.ts
@@ -1,10 +1,26 @@
-import express, { Request, Response } from 'express';
-import { all } from '../models/destination';
+import express from 'express';
+import { all, like } from '../models/destination';
 const router = express.Router();
 
 router.get('/all/', async function (req, res) {
   const dests = await all();
   res.send(dests);
+});
+
+router.get(['/like/:text', '/like/'], async function (req, res) {
+  const RETURN_COUNT = 10;
+  const DISTANCE_THRESH = 2;
+
+  const text = req.params.text;
+  console.log(text);
+  if (!text) {
+    res.send([]);
+    return;
+  }
+
+  const rows = await like(text, DISTANCE_THRESH, RETURN_COUNT);
+
+  res.send(rows);
 });
 
 export { router };
