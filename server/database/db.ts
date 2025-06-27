a01aac85324f98c2a51fd4cab61f6ebbe2526a91
itsmeox <ongxuan1028@gmail.com>
2025-06-27 11:17:53 +0800

    Change import

--- server/database/db.ts
+++ server/database/db.ts
@@ -1,19 +1,20 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
+import { createPool } from 'mysql2';
 
-const pool = mysql
-  .createPool({
-    host: 'localhost',
-    user: 'ItsMeOX',
-    password: 'password',
-    database: 'ESC',
-  })
-  .promise();
+const pool = createPool({
+  host: 'localhost',
+  user: 'ItsMeOX',
+  password: 'password',
+  database: 'ESC',
+}).promise();
 
 async function cleanup() {
   await pool.end();
 }
 
-export { pool, cleanup };
+async function sync() {}
+
+export { pool, cleanup, sync };
 
 // CREATE USER 'your_username'@'your_host' IDENTIFIED BY 'your_password';
 // GRANT ALL PRIVILEGES ON db_name.* TO 'your_username'@'localhost';
