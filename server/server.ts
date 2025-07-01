5bb49058032a6ba63bb8985fad0e0e4a79f96847
ItsMeOX <ongxuan1028@gmail.com>
2025-07-01 16:45:16 +0800

    Add routers for hotel and hotel price fetching

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,8 @@ import process from 'process';
 import { cleanup } from './database/db';
 
 import { router as destRouter } from './routes/destination';
+import { router as priceRouter } from './routes/hotel-price';
+import { router as hotelRouter } from './routes/hotel';
 
 import { sync as syncDest } from './models/destination';
 
@@ -18,6 +20,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/destination', destRouter);
+app.use('/api/hotel-price', priceRouter);
+app.use('/api/hotel', hotelRouter);
 
 app.listen(5000, () => {
   console.log('Server listening on port 5000.');
