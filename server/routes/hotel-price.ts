e328d392b12c8a7507d307796df43d4a4c7b82df
ItsMeOX <ongxuan1028@gmail.com>
2025-07-01 16:44:38 +0800

    Add hotel price route

---
+++ server/routes/hotel-price.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { PriceResponse } from '../../types/Price';
+const router = express.Router();
+
+router.get('/query/:dest_id', async (req, res) => {
+  const dest_id = req.params.dest_id;
+  if (!dest_id) {
+    res.send([]);
+    return;
+  }
+
+  const url = `https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=${dest_id}&checkin=2025-10-01&checkout=2025-10-07&lang=en_US&currency=SGD&country_code=SG&guests=2&partner_id=1`;
+
+  const response = await fetch(url);
+  const data: PriceResponse = await response.json();
+  console.log(data);
+
+  res.send(data);
+});
+
+export { router };
