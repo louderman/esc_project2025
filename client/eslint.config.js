dbd8df5a603fbf5c0c9ef861a2f5e3067d5a0932
itsmeox <ongxuan1028@gmail.com>
2025-07-09 17:44:01 +0800

    Remove node_modules

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -25,27 +24,5 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
-  },
-)
-=======
-import js from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import { defineConfig } from 'eslint/config';
-
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser },
-    extends: ['plugin:react/jsx-runtime'],
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
->>>>>>> oldrepo/master
+  }
+);
