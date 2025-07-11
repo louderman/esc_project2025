89e2b7fdc991d4f2a231346f24dbf876204fdd6f
ItsMeOX <ongxuan1028@gmail.com>
2025-07-11 21:28:47 +0800

    Implement useSortedHotels hook

---
+++ client/src/hooks/hotels/useSortedHotels.ts
@@ -0,0 +1,33 @@
+import { useMemo } from 'react';
+import type { Hotel } from '../../../../types/Hotel';
+import {
+  SORT_OPTIONS,
+  type SortByOptions,
+} from '../../reducers/listingReducer';
+import type { Price } from '../../../../types/Price';
+
+export function useSortedHotels(
+  pricedHotels: (Hotel & Price)[],
+  sortOptions: SortByOptions
+) {
+  const sortedHotels = useMemo(() => {
+    const hotelsCopy = [...pricedHotels];
+    switch (sortOptions) {
+      case SORT_OPTIONS.PRICE_ASC:
+        hotelsCopy.sort((a, b) => a.price - b.price);
+        break;
+      case SORT_OPTIONS.PRICE_DESC:
+        hotelsCopy.sort((a, b) => b.price - a.price);
+        break;
+      case SORT_OPTIONS.RATING_ASC:
+        hotelsCopy.sort((a, b) => a.rating - b.rating);
+        break;
+      case SORT_OPTIONS.RATING_DESC:
+        hotelsCopy.sort((a, b) => b.rating - a.rating);
+        break;
+    }
+    return hotelsCopy;
+  }, [pricedHotels, sortOptions]);
+
+  return sortedHotels;
+}
