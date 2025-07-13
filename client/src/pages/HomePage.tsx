f2cf69e01b4792336080cdfc294b35f401ab212c
Roger Yue III <1007850@mymail.sutd.edu.sg>
2025-07-13 16:35:45 +0800

    base UI

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,24 +1,9 @@
-import { useCallback, useEffect, useReducer, useRef, useState } from 'react';
-import SearchBar from '../components/listing/SearchBar/SearchBar';
-import styles from './listingpage.module.css';
-import type { StayDatesState } from '../components/listing/SearchBar/DateInput/DateInput';
-import type { OccupancyState } from '../components/listing/SearchBar/GuestInput/GuestInput';
-import FilterPanel from '../components/listing/ListingControl/FilterPanel';
-import Listings from '../components/listing/Listings/Listings';
-import {
-  initialListingState,
-  listingReducer,
-} from '../reducers/listingReducer';
-import type { Hotel } from '../../../types/Hotel';
-import type { Price, PriceResponse } from '../../../types/Price';
-import { usePollingAsync } from '../hooks/usePollingAsync';
-import { useNavigate } from 'react-router-dom';
-import { usePricedHotels } from '../hooks/hotels/usePricedHotels';
-import { useFilteredHotels } from '../hooks/hotels/useFilteredHotels';
-import SortPanel from '../components/listing/ListingControl/SortPanel';
-import { useSortedHotels } from '../hooks/hotels/useSortedHotels';
-import { useUrlSync } from '../hooks/url/useUrlSync';
+import { useState } from 'react';
+import SearchBar from '../components/homepage/SearchBar/SearchBar';
+import type { StayDatesState } from '../components/homepage/SearchBar/DateInput/DateInput';
+import type { OccupancyState } from '../components/homepage/SearchBar/GuestInput/GuestInput';
 
+import styles from './homepage.module.css';
 
 
 
@@ -37,15 +22,18 @@ export default function HomePage() {
   });
 
   return (
-    <div>
-      <SearchBar
-        userDest={userDest}
-        setUserDest={setUserDest}
-        stayDates={stayDates}
-        setStayDates={setStayDates}
-        occupancy={occupancy}
-        setOccupancy={setOccupancy}
-      />
+    <div className={styles.container}>
+      <div className={styles.searchbarSection}>
+        <div className={styles.headerQuestion}>Looking for a place to stay?</div>
+        <SearchBar
+          userDest={userDest}
+          setUserDest={setUserDest}
+          stayDates={stayDates}
+          setStayDates={setStayDates}
+          occupancy={occupancy}
+          setOccupancy={setOccupancy}
+        />
+      </div>
     </div>
   );
 }
