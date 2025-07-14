ea5b0ac87c3aa5247c7c44430c183d321ce1cba8
itsmeox <ongxuan1028@gmail.com>
2025-07-15 01:39:22 +0800

    Change hotel price route params to queries, Allow querying by URL queries

--- server/routes/hotel-price.ts
+++ server/routes/hotel-price.ts
@@ -2,20 +2,31 @@ import express from 'express';
 import { PriceResponse } from '../../types/Price';
 const router = express.Router();
 
-router.get(['/query', '/query/:dest_id'], async (req, res) => {
-  const dest_id = req.params.dest_id;
-  if (!dest_id) {
-    res.send({
-      searchCompleted: null,
-      completed: true,
-      status: null,
-      currency: null,
-      hotels: [],
-    });
+/**
+ * GET /query?dest_id={dest_id}&checkin={checkin}&checkout={checkout}&guests={guests}
+ */
+router.get('/query', async (req, res) => {
+  const { dest_id, checkin, checkout, guests } = req.query;
+  if (!dest_id || !checkin || !checkout || !guests) {
+    res.status(400).json({ error: 'Missing one of the required parameters.' });
     return;
   }
 
-  const url = `https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=${dest_id}&checkin=2025-10-01&checkout=2025-10-07&lang=en_US&currency=SGD&country_code=SG&guests=2&partner_id=1`;
+  const checkinDate = new Date(checkin as string);
+  const checkoutDate = new Date(checkout as string);
+  const guestCount = parseInt(guests as string);
+
+  if (
+    isNaN(checkinDate.getTime()) ||
+    isNaN(checkoutDate.getTime()) ||
+    isNaN(guestCount)
+  ) {
+    res.status(400).json({ error: 'Invalid date or guest count' });
+  }
+
+  const url = `https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=${dest_id}&checkin=${checkin}&checkout=${checkout}&lang=en_US&currency=SGD&country_code=SG&guests=${guestCount}&partner_id=1`;
+  // console.log(url);
+  // const url = `https://hotelapi.loyalty.dev/api/hotels/prices?destination_id=${dest_id}&checkin=2025-10-01&checkout=2025-10-07&lang=en_US&currency=SGD&country_code=SG&guests=2&partner_id=1`;
 
   const response = await fetch(url);
   const data: PriceResponse = await response.json();
