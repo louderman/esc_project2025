57d5a65711a281bf36e693dcd053a1a3ead285f9
itsmeox <ongxuan1028@gmail.com>
2025-07-24 00:10:57 +0800

    Add jest for backend testing, Add destination API tests

--- server/server.ts
+++ server/server.ts
@@ -30,3 +30,5 @@ app.use('/api/auth', authRouter);
 app.listen(5000, () => {
   console.log('Server listening on port 5000.');
 });
+
+export default app;
---
+++ server/test/destination.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import { cleanup } from '../database/db';
+import app from '../server';
+
+afterAll(async () => {
+  await cleanup();
+});
+
+describe('GET /api/destination/random?count={count}', () => {
+  it('Test returned destination length matches count', async () => {
+    const res = await request(app).get('/api/destination/random?count=5');
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveLength(5);
+  });
+  it('Test zero return count', async () => {
+    const res = await request(app).get('/api/destination/random?count=0');
+    expect(res.statusCode).toBe(400);
+  });
+  it('Test negative return count', async () => {
+    const res = await request(app).get('/api/destination/random?count=-1');
+    expect(res.statusCode).toBe(400);
+  });
+  it('Test invalid return count', async () => {
+    const res = await request(app).get('/api/destination/random?count=abc');
+    expect(res.statusCode).toBe(400);
+  });
+  it('Test missing return count', async () => {
+    const res = await request(app).get('/api/destination/random');
+    expect(res.statusCode).toBe(400);
+  });
+});
