480d2b6ddf3d615d55df39cf672cb3cbdd090c7e
itsmeox <ongxuan1028@gmail.com>
2025-07-24 00:12:35 +0800

    Add route to query destinations with lng&lat, Rename function names, Return 400 on invalid count param

--- server/routes/destination.ts
+++ server/routes/destination.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { all, query, random } from '../models/destination';
+import {
+  getAllDestinations,
+  getRandomDestinations,
+  searchDestinations,
+  searchDestinationsInBounds,
+} from '../models/destination';
 const router = express.Router();
 
 router.get('/all/', async function (req, res) {
-  const dests = await all();
+  const dests = await getAllDestinations();
   res.send(dests);
 });
 
@@ -11,15 +16,15 @@ router.get('/all/', async function (req, res) {
  * GET /random?count={count}
  */
 router.get('/random', async function (req, res) {
-  const countRaw = parseInt(req.query.count as string);
-  const count = isNaN(countRaw) ? 10 : countRaw;
-
-  if (count === 0) {
-    res.send([]);
-    return;
+  const countRaw = req.query.count as string;
+  const count = parseInt(countRaw);
+  if (!countRaw || isNaN(count) || count < 1) {
+    return res
+      .status(400)
+      .json({ error: 'Invalid count parameter. Must be a positive number.' });
   }
 
-  const dests = await random(count);
+  const dests = await getRandomDestinations(count);
   res.send(dests);
 });
 
@@ -35,9 +40,35 @@ router.get(['/query/:text', '/query/'], async function (req, res) {
     return;
   }
 
-  const rows = await query(text, DISTANCE_THRESH, count);
+  const rows = await searchDestinations(text, DISTANCE_THRESH, count);
 
   res.send(rows);
+});
+
+/**
+ * GET /query/bounds?minLat={...}&maxLat={...}&minLng={...}&maxLng={...}
+ */
+router.get(['/bounds'], async function (req, res) {
+  console.log('called');
+  const lat1 = parseFloat(req.query.minLat as string);
+  const lat2 = parseFloat(req.query.maxLat as string);
+  const lng1 = parseFloat(req.query.minLng as string);
+  const lng2 = parseFloat(req.query.maxLng as string);
+  if (isNaN(lat1) || isNaN(lat2) || isNaN(lng1) || isNaN(lng2)) {
+    res.status(400).json({ error: 'Invalid or missing bounds parameters.' });
+    return;
+  }
+  const [minLat, maxLat] = lat1 < lat2 ? [lat1, lat2] : [lat2, lat1];
+  const [minLng, maxLng] = lng1 < lng2 ? [lng1, lng2] : [lng2, lng1];
+  console.log(minLat, maxLat, minLng, maxLng);
+  const destinations = await searchDestinationsInBounds({
+    minLat,
+    maxLat,
+    minLng,
+    maxLng,
+  });
+
+  res.send(destinations);
 });
 
 export { router };
