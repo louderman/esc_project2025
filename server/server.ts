49eac279336b2a33cdf9d82f8f5ccb439e92cc39
itsmeox <ongxuan1028@gmail.com>
2025-07-25 10:55:59 +0800

    Set NODE_ENV env variable for testing

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,18 @@
 import process from 'process';
+import express from 'express';
+import cors from 'cors';
+
 import { cleanup } from './database/db';
 
 import { sync as syncDest } from './models/destination';
 import { sync as syncUser } from './models/userModel';
-import app from './app';
 
-// I have to separate server.ts into
-// app.ts and server.ts
-// for testing (app.listen() shouldnt be invoked during tests)
+import { router as destRouter } from './routes/destination';
+import { router as priceRouter } from './routes/hotel-price';
+import { router as hotelRouter } from './routes/hotel';
+import { router as authRouter } from './routes/auth';
+
+const app = express();
 
 process.on('SIGINT', cleanup);
 process.on('SIGTERM', cleanup);
@@ -15,6 +20,18 @@ process.on('SIGTERM', cleanup);
 syncDest();
 syncUser();
 
-app.listen(5000, () => {
-  console.log('Server listening on port 5000.');
-});
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/destination', destRouter);
+app.use('/api/hotel-price', priceRouter);
+app.use('/api/hotel', hotelRouter);
+app.use('/api/auth', authRouter);
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(5000, () => {
+    console.log('Server listening on port 5000.');
+  });
+}
+
+export default app;
