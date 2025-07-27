e401d6d72877333b4c3dd01cb092657e12440177
r3poo <roger@r3po.org>
2025-07-27 19:27:13 +0800

    update page design, implemented navigation to listing page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from './homepage.module.css';
 import type { StayDatesState } from '../components/listing/SearchBar/DateInput/DateInput';
 import type { OccupancyState } from '../components/listing/SearchBar/GuestInput/GuestInput';
 import SearchBar from '../components/listing/SearchBar/SearchBar';
 import type { DestinationState } from '../components/listing/SearchBar/DestinationInput/DestinationInput';
+import type { Destination } from '../../../types/Destination';
 import { useNavigate } from 'react-router-dom';
 import { useSearchBarUrlSync } from '../hooks/url/useSearchBarUrlSync';
+
+import DestinationCard from '../components/homepage/DestinationCard';
+
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -15,9 +19,13 @@ export default function HomePage() {
     id: '',
     name: '',
   });
+  let today: Date = new Date();
+  let tomorrow: Date = new Date();
+  today.setDate(new Date().getDate()+1)
+  tomorrow.setDate(new Date().getDate()+2)
   const [stayDates, setStayDates] = useState<StayDatesState>({
-    checkinDate: null,
-    checkoutDate: null,
+    checkinDate: today,
+    checkoutDate: tomorrow,
   });
   const [occupancy, setOccupancy] = useState<OccupancyState>({
     adults: 1,
@@ -35,13 +43,28 @@ export default function HomePage() {
     navigate,
   });
 
-  function handleSearchHotel() {
-    // Handle empty user inputs here
-    //
-    //
-    // TODO^
 
-    syncSearchBarToURL('/listing');
+  const [suggestedDests, setSuggestedDests] = useState<Destination[]>([]);
+  useEffect(() => {
+    async function fetchRandomSuggestions() {
+      let url = `/api/destination/random?count=8`;
+      const res = await fetch(url, { method: 'GET' });
+      const dests: Destination[] = await res.json();
+      setSuggestedDests(dests)
+    }
+    fetchRandomSuggestions()
+  }, []);
+  
+  function handleSuggestionClick(dest: Destination) {
+    let destination: DestinationState = { id: dest.dest_id, name: dest.term };
+    setDestination(destination)
+    syncSearchBarToURL('/listing')
+  }
+
+  function handleSearchHotel() {
+    if (destination.id=='') { return; }
+    // navigate('/listing', {state: {destination: destination, occupancy: occupancy, stayDates: stayDates}})
+    syncSearchBarToURL('/listing')
   }
 
   return (
@@ -60,6 +83,48 @@ export default function HomePage() {
           onSubmit={handleSearchHotel}
         />
       </div>
+      
+      
+      <div className={styles.destinationSuggestion}>
+        <div className={styles.destinationSuggestionHeader}>
+          Find Hotels in These Cities
+        </div>
+        <div className={styles.destinationSuggestionSubHeader}>
+          Explore the world at the lowest cost!
+        </div>
+        <div className={styles.destinationSuggestionsBox}>
+          {suggestedDests.map((dest, index) => (
+            <div className={styles.destinationSuggestionsItem}>
+              <DestinationCard
+                url='https://d2ey9sqrvkqdfs.cloudfront.net/5CCH/0.jpg'
+                dest={dest}
+                onclick={handleSuggestionClick}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      
+
+      <footer className={styles.footer}>
+        <div className={styles.qualitiesHeader}>
+          Why Choose Us
+        </div>
+        <div className={styles.qualitiesBox}>
+          <div className={styles.qualityItem}>
+            Destinations from across the world
+          </div>
+          <div className={styles.qualityItem}>
+            Experienced agents
+          </div>
+          <div className={styles.qualityItem}>
+            Buy or rent your home
+          </div>
+          <div className={styles.qualityItem}>
+            Cheapest prices available
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
