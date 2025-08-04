be41c9745978c3559f6f1e4b0a21d02c7be56d46
itsmeox <ongxuan1028@gmail.com>
2025-08-04 20:37:54 +0800

    Add test cases for DateInput

---
+++ client/src/components/listing/SearchBar/DateInput/DateInput.test.tsx
@@ -0,0 +1,101 @@
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { useState } from 'react';
+import { afterEach, describe, expect, it } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import userEvent from '@testing-library/user-event';
+import type { StayDatesState } from './DateInput';
+import DateInput from './DateInput';
+
+afterEach(() => {
+  cleanup();
+});
+
+const formatDate = (date: Date) => {
+  return new Intl.DateTimeFormat('en-US', {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+  }).format(date);
+};
+const today = new Date();
+today.setHours(0, 0, 0, 0);
+
+function DateInputWrapper() {
+  const [stayDates, setStayDates] = useState<StayDatesState>({
+    checkinDate: null,
+    checkoutDate: null,
+  });
+
+  return <DateInput stayDates={stayDates} setStayDates={setStayDates} />;
+}
+
+describe('DateInput', () => {
+  it('Test should render default text and open calendar on click', async () => {
+    render(<DateInputWrapper />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent(/select check in/);
+
+    await userEvent.click(button);
+    const calendarPages = await screen.findAllByTestId('calendar-page');
+    expect(calendarPages.length).toBe(2);
+    expect(calendarPages[0]).toBeInTheDocument();
+    expect(calendarPages[1]).toBeInTheDocument();
+  });
+
+  it('Test should display correct check-in, check-out and number of nights', async () => {
+    render(<DateInputWrapper />);
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+
+    // Click today's cell
+    const todayCell = screen
+      .getAllByTestId('cell')
+      .find((cell) => cell.textContent === String(today.getDate()));
+    expect(todayCell).toBeDefined();
+    await userEvent.click(todayCell!);
+
+    // Click next month button
+    const nextBtn = screen.getByTestId('next-btn');
+    expect(nextBtn).toBeDefined();
+    await userEvent.click(nextBtn);
+
+    // Select future date
+    const futureCell = screen
+      .getAllByTestId('cell')
+      .find((cell) => cell.textContent === '10');
+    expect(futureCell).toBeDefined();
+    await userEvent.click(futureCell!);
+
+    const checkoutDate = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      10
+    );
+    const formattedCheckIn = formatDate(today);
+    const formattedCheckOut = formatDate(checkoutDate);
+    const nights = Math.round(
+      (checkoutDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    await userEvent.click(document.body);
+    console.log('formattedCheckin', formattedCheckIn);
+    expect(screen.getByText(new RegExp(formattedCheckIn))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(formattedCheckOut))).toBeInTheDocument();
+    expect(
+      screen.getByText(new RegExp(`\\(${nights} nights\\)`))
+    ).toBeInTheDocument();
+  });
+
+  it('Test should close calendar when clicking outside', async () => {
+    render(<DateInputWrapper />);
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+    const calendarPages = await screen.findAllByTestId('calendar-page');
+    expect(calendarPages.length).toBe(2);
+
+    await userEvent.click(document.body);
+
+    await waitFor(() => {
+      expect(screen.queryAllByTestId('calendar-page')).toHaveLength(0);
+    });
+  });
+});
