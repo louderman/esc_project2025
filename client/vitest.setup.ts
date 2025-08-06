1becebe500ed8ae9cd524be41cac7282c4fad8a6
itsmeox <ongxuan1028@gmail.com>
2025-08-06 16:16:22 +0800

    Add vitest setup file

---
+++ client/vitest.setup.ts
@@ -0,0 +1,8 @@
+// Mock resize observer, otherwise would throw errro
+(global as any).ResizeObserver = class {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+global.ResizeObserver = ResizeObserver;
