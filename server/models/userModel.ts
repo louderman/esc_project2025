83a323d319e27791bff837e145cecd35d8d2b9db
itsmeox <ongxuan1028@gmail.com>
2025-08-14 04:37:06 +0800

    Add bycrypt library to hash password

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,10 +1,11 @@
 import { pool } from '../database/db';
+import bcrypt from 'bcrypt';
 
 const tableName = 'users';
 // CREATE TABLE IF NOT EXISTS Users (id INT AUTO_INCREMENT PRIMARY KEY,name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL UNIQUE,password VARCHAR(255) NOT NULL);
 
-function sync() {
-  return pool.query(`
+async function sync() {
+  return await pool.query(`
     CREATE TABLE IF NOT EXISTS ${tableName} (
       id INT AUTO_INCREMENT PRIMARY KEY,
       name VARCHAR(100) NOT NULL,
@@ -14,19 +15,24 @@ function sync() {
   `);
 }
 
-function insertUser(name: string, email: string, password: string) {
+async function insertUser(name: string, email: string, password: string) {
   // Trim and validate inputs
   const trimmedName = name?.trim();
   const trimmedEmail = email?.trim();
   const trimmedPassword = password?.trim();
 
   if (!trimmedName || !trimmedEmail || !trimmedPassword) {
-    throw new Error('All fields (name, email, password) must be non-empty and trimmed.');
+    throw new Error(
+      'All fields (name, email, password) must be non-empty and trimmed.'
+    );
   }
+
+  const saltRound = 10;
+  const hashedPassword = await bcrypt.hash(trimmedPassword, saltRound);
 
   return pool.query(
     `INSERT INTO ${tableName} (name, email, password) VALUES (?, ?, ?)`,
-    [trimmedName, trimmedEmail, trimmedPassword]
+    [trimmedName, trimmedEmail, hashedPassword]
   );
 }
 
